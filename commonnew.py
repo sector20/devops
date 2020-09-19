4ace48ab3e41cf275bbf0f3aa701b53d48285ba3
sector20 <31368017+sector20@users.noreply.github.com>
2020-09-19 22:38:17 +0530

    Update: ADB groupSumAgg common.py commonnew.py notebooks test1.txt

--- commonnew.py
+++ commonnew.py
@@ -1,17 +1,26 @@
 # Databricks notebook source
-def commonfunnew():
-  file_location = "/FileStore/tables/googleplaystore.csv"
-  file_type = "csv"
-  #amit
-  # CSV options
-  infer_schema = "false"
-  first_row_is_header = "true"
-  delimiter = ","
+dbutils.widgets.dropdown("X123", "1", [str(x) for x in range(1, 10)])
 
-  # The applied options are for CSV files. For other file types, these will be ignored.
-  dfamitnew = spark.read.format(file_type) \
-    .option("inferSchema", infer_schema) \
-    .option("header", first_row_is_header) \
-    .option("sep", delimiter) \
-    .load(file_location)
-  return dfamitnew
+dbutils.widgets.get("X123")
+
+# COMMAND ----------
+
+#from pyspark.sql.functions import sum
+
+#file_location = "/FileStore/tables/googleplaystore.csv"
+#file_type = "csv"
+##amit
+## CSV options
+#infer_schema = "false"
+#first_row_is_header = "true"
+#delimiter = ","
+
+## The applied options are for CSV files. For other file types, these will be ignored.
+#dfamitnew = spark.read.format(file_type) \
+#  .option("inferSchema", infer_schema) \
+#  .option("header", first_row_is_header) \
+#  .option("sep", delimiter) \
+#  .load(file_location)
+
+#df5 = dfamitnew
+#display(df5.select('category','rating').where(df5.Rating != 'NaN').groupBy('category').agg(sum('Rating')))
