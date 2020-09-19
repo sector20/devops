e3db7df251127bdca410a993a53b8967bd5b050f
sector20 <31368017+sector20@users.noreply.github.com>
2020-09-19 20:24:34 +0530

    Update: ADB groupSumAgg common.py commonnew.py notebooks test1.txt

--- commonnew.py
+++ commonnew.py
@@ -1,12 +1,5 @@
 # Databricks notebook source
-dbutils.widgets.dropdown("X123", "1", [str(x) for x in range(1, 10)])
-
-dbutils.widgets.get("X123")
-
-
-# COMMAND ----------
-
-def commonfun():
+def commonfunnew():
   file_location = "/FileStore/tables/googleplaystore.csv"
   file_type = "csv"
   #amit
@@ -16,9 +9,9 @@ def commonfun():
   delimiter = ","
 
   # The applied options are for CSV files. For other file types, these will be ignored.
-  dfamit = spark.read.format(file_type) \
+  dfamitnew = spark.read.format(file_type) \
     .option("inferSchema", infer_schema) \
     .option("header", first_row_is_header) \
     .option("sep", delimiter) \
     .load(file_location)
-  return dfamit
+  return dfamitnew
