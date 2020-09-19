f020a91810226e033d3a7738ddcb853815ded404
sector20 <31368017+sector20@users.noreply.github.com>
2020-09-19 20:16:07 +0530

    Update: ADB groupSumAgg common.py notebooks test1.txt

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 dbutils.widgets.dropdown("X123", "1", [str(x) for x in range(1, 10)])
 
-dbutils.widgets.get("X123")
+#dbutils.widgets.get("X123")
 
 
 # COMMAND ----------
@@ -21,4 +21,4 @@ def commonfun():
     .option("header", first_row_is_header) \
     .option("sep", delimiter) \
     .load(file_location)
-  return dfamit
+  return dbutils.widgets.get("X123")
